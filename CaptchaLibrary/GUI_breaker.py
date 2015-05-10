f3255fa3f30d3f41e7bb49881b205be233660edd
JunoJunho <wnsgh611@gmail.com>
2015-05-11 03:52:55 +0900

    my_breaker 수정 및 GUI_breaker.py 파일 생성

---
+++ CaptchaLibrary/GUI_breaker.py
@@ -0,0 +1,5 @@
+from Tkinter import *
+
+
+if __name__ == 'main':
+    print "Python Tk GUI"
--- my_breaker.py
+++ my_breaker.py
@@ -3,7 +3,6 @@ from CaptchaLibrary.CHARACTERIZER.CHARACTERIZER import CHARACTERIZER as Guesser
 import os
 
 if __name__ == '__main__':
-    dir = os.path.dirname(__file__)
     init_theta1, init_theta2 = INITIALIZATION('D:\\003. Project\CS448\CAPTCHA\CaptchaLibrary\\nn_params.txt')
     guess_string = Guesser(init_theta1, init_theta2, 'D:\\003. Project\CS448\CAPTCHA\CaptchaLibrary\\Captcha1.jpg')
     print guess_string
